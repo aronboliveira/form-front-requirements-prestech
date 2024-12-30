0aa2fab812be3d4f9a56b225493f35fcfdae0199
Aron-PC <aron.b.96@gmail.com>
2024-12-30 17:26:41 -0300

    include empty python script for generating spreadsheet

---
+++ create_form_descriptor.py
@@ -0,0 +1,14 @@
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Font, Alignment
+from openpyxl.utils import get_column_letter
+from colorama import Fore, init
+init(autoreset=True)
+def create_form_descriptor() -> None:
+  return None
+
+
+if __name__ == '__main__':
+  create_form_descriptor()
+else: 
+  print(Fore.RED + 'Directory not authorized for execution')
