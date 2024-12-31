9f0816c6cd6447c944190d1cf5c0590863a8694d
Aron-PC <aron.b.96@gmail.com>
2024-12-31 15:15:24 -0300

    include early return for empty strings in the IOHandler

--- src/lib/client/handlers/IOHandler.ts
+++ src/lib/client/handlers/IOHandler.ts
@@ -1,16 +1,67 @@
 import { DDDPattern, TelType } from "@/lib/definitions/helpers";
 import MathHandler from "./MathHandler";
 export default class IOHandler {
-  static applyUpperCase(v: string, f: Function, limit?: number): void {
-    limit ? v.length === limit && f(v.toUpperCase()) : f(v.toUpperCase());
+  static adjustTelCountryCode(code: string): string {
+    if (code === "") return code;
+    code = code.trim().replace(/^[0-9\+]/g, "");
+    if (!code.startsWith("+")) code = `+${code}`;
+    if (code.length > 4) code = code.slice(0, 4);
+    return code;
+  }
+  static adjustTelDDD(ddd: string): DDDPattern {
+    if (ddd === "") return ddd as DDDPattern;
+    ddd = ddd.trim().replace(/[^0-9]/g, "");
+    if (ddd.length > 2) ddd = ddd.slice(0, 2);
+    if (ddd.startsWith("1") && ddd.endsWith("0")) ddd = "11";
+    else if (ddd.startsWith("2")) {
+      if (ddd.endsWith("0")) ddd = "21";
+      else if (ddd.endsWith("3")) ddd = "24";
+      else if (ddd.endsWith("5") || ddd.endsWith("6")) ddd = "27";
+      else if (ddd.endsWith("9")) ddd = "31";
+    } else if (ddd.startsWith("3")) {
+      if (ddd.endsWith("0")) ddd = "31";
+      else if (ddd.endsWith("6")) ddd = "37";
+      else if (ddd.endsWith("9")) ddd = "41";
+    } else if (ddd.startsWith("4") && ddd.endsWith("0")) ddd = "41";
+    else if (ddd.startsWith("5")) {
+      if (ddd.endsWith("0")) ddd = "51";
+      else if (ddd.endsWith("2")) ddd = "53";
+      else if (MathHandler.parseNotNaN(ddd.slice(-1), 11, "int") > 5)
+        ddd = "61";
+    } else if (ddd.startsWith("6") && ddd.endsWith("0")) ddd = "61";
+    else if (
+      ddd.startsWith("7") &&
+      MathHandler.parseNotNaN(ddd.slice(-1), 1, "int") % 2 === 0
+    )
+      ddd = (MathHandler.parseNotNaN(ddd, 11, "int") + 1).toString();
+    if (ddd.startsWith("8") && ddd.endsWith("0")) ddd = "81";
+    else if (ddd.startsWith("9") && ddd.endsWith("0")) ddd = "91";
+    return ddd as DDDPattern;
+  }
+  static applyAgeContrainst(age: string): string {
+    if (age === "") return age;
+    const iniAge = age;
+    age = age.trim().replace(/[^0-9]*/g, "");
+    if (age.length > 3) age = age.slice(0, 3);
+    if (
+      MathHandler.parseNotNaN(
+        age,
+        MathHandler.parseNotNaN(iniAge, 1, "int"),
+        "int"
+      ) > 127
+    )
+      age = "127";
+    return age;
   }
   static applyEmailExtension(emailValue: string): string {
-    emailValue = emailValue.trim();
-    if (emailValue === "") emailValue = "@.";
-    else if (emailValue === "@") emailValue += ".";
+    if (emailValue !== "") {
+      emailValue = emailValue.trim();
+      if (emailValue === "@") emailValue += ".";
+    } else emailValue = "@.";
     return emailValue;
   }
   static applyTelExtension(telValue: string, type: TelType): string {
+    if (telValue === "") return telValue;
     telValue = telValue.trim().replaceAll(/[^0-9\-\+]/g, "");
     if (type === "local") {
       if (
@@ -46,39 +97,7 @@ export default class IOHandler {
     }
     return telValue;
   }
-  static adjustTelDDD(ddd: string): DDDPattern {
-    ddd = ddd.trim().replace(/[^0-9]/g, "");
-    if (ddd.length > 2) ddd = ddd.slice(0, 2);
-    if (ddd.startsWith("1") && ddd.endsWith("0")) ddd = "11";
-    else if (ddd.startsWith("2")) {
-      if (ddd.endsWith("0")) ddd = "21";
-      else if (ddd.endsWith("3")) ddd = "24";
-      else if (ddd.endsWith("5") || ddd.endsWith("6")) ddd = "27";
-      else if (ddd.endsWith("9")) ddd = "31";
-    } else if (ddd.startsWith("3")) {
-      if (ddd.endsWith("0")) ddd = "31";
-      else if (ddd.endsWith("6")) ddd = "37";
-      else if (ddd.endsWith("9")) ddd = "41";
-    } else if (ddd.startsWith("4") && ddd.endsWith("0")) ddd = "41";
-    else if (ddd.startsWith("5")) {
-      if (ddd.endsWith("0")) ddd = "51";
-      else if (ddd.endsWith("2")) ddd = "53";
-      else if (MathHandler.parseNotNaN(ddd.slice(-1), 11, "int") > 5)
-        ddd = "61";
-    } else if (ddd.startsWith("6") && ddd.endsWith("0")) ddd = "61";
-    else if (
-      ddd.startsWith("7") &&
-      MathHandler.parseNotNaN(ddd.slice(-1), 1, "int") % 2 === 0
-    )
-      ddd = (MathHandler.parseNotNaN(ddd, 11, "int") + 1).toString();
-    if (ddd.startsWith("8") && ddd.endsWith("0")) ddd = "81";
-    else if (ddd.startsWith("9") && ddd.endsWith("0")) ddd = "91";
-    return ddd as DDDPattern;
-  }
-  static adjustTelCountryCode(code: string): string {
-    code = code.trim().replace(/^[0-9\+]/g, "");
-    if (!code.startsWith("+")) code = `+${code}`;
-    if (code.length > 4) code = code.slice(0, 4);
-    return code;
+  static applyUpperCase(v: string, f: Function, limit?: number): void {
+    limit ? v.length === limit && f(v.toUpperCase()) : f(v.toUpperCase());
   }
 }
