50bc5057de20499084a9bc4bbe01d58bebd0dadb
aronboliveira <aronboliveira@nutelladeveloper.Novaprestech.INT>
2025-01-07 13:03:43 -0300

    include text classes + base string vars

---
+++ src/lib/client/vars.ts
@@ -0,0 +1,16 @@
+const inps = `form-control`,
+texts = `${inps} form-text-area`,
+btn = `btn`;
+export const classes = {
+  inpDivClasses: `mb-3 inpDivClasses`,
+  inpLabClasses: `form-label`,
+  inpClasses: `${inps}`,
+  textClasses: `${texts}`,
+  contextualTextClasses: `${texts} contextualText`,
+  ccClasses: `${inps} cc linked`,
+  dddClasses: `${inps} ddd linked`,
+  telClasses: `${inps} tel`,
+  selectClasses: `form-select`,
+  btnSec: `${btn} btn-secondary`,
+  btnPrim: `${btn} btn-primary`,
+};
