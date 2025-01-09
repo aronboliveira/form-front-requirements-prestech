8b3efe7001c5c623adcda9dd5e389032657522ac
aronboliveira <aronboliveira@nutelladeveloper.Novaprestech.INT>
2025-01-09 11:46:39 -0300

    removed all from staging area

--- src/lib/helpers/StringHelper.ts
+++
@@ -1,9 +0,0 @@
-export default class StringHelper {
-  static camelToSnake(v: string): string {
-    if (!v || (typeof v === "string" && !/[A-Z]/.test(v))) return v;
-    return v.replace(
-      /([a-z])([A-Z])/g,
-      (_, l, u) => `${l.toLowerCase()}_${u.toLowerCase()}`
-    );
-  }
-}
