ca8e56180ed3f3d04d39d5be6fff72ffb8ca5647
Aron-PC <aron.b.96@gmail.com>
2025-01-10 13:24:23 -0300

    refactor syntax for compabilityvalidator

--- src/lib/client/validators/CompabilityValidator.ts
+++ src/lib/client/validators/CompabilityValidator.ts
@@ -1,4 +1,15 @@
 export default class CompabilityValidator {
+  static isChromium(): boolean {
+    return navigator.userAgentData
+      ? navigator.userAgentData.brands.some(brand =>
+          ["chrome", "edge", "samsung", "opera", "vivaldi", "brave"].some(
+            name => brand.brand.toLowerCase().includes(name)
+          )
+        )
+      : /Chrome|CriOS|EdgA|Edg|SamsungBrowser|OPR|Vivaldi|Brave/i.test(
+          navigator.userAgent
+        );
+  }
   static isSafari(): boolean {
     if (navigator.userAgentData)
       return navigator.userAgentData.brands.some(brand =>
@@ -13,35 +24,17 @@ export default class CompabilityValidator {
         : false;
     }
   }
-  static isChromium(): boolean {
-    if (navigator.userAgentData) {
-      const chromiumBrands = [
-        "chrome",
-        "edge",
-        "samsung",
-        "opera",
-        "vivaldi",
-        "brave",
-      ];
-      return navigator.userAgentData.brands.some(brand =>
-        chromiumBrands.some(name => brand.brand.toLowerCase().includes(name))
-      );
-    }
-    return /Chrome|CriOS|EdgA|Edg|SamsungBrowser|OPR|Vivaldi|Brave/i.test(
-      navigator.userAgent
-    );
+  static isWebkit(): boolean {
+    return CompabilityValidator.isChromium() || CompabilityValidator.isSafari();
   }
   static isFirefox(): boolean {
-    if (navigator.userAgentData)
-      return navigator.userAgentData.brands.some(brand =>
-        brand.brand.toLowerCase().includes("firefox")
-      );
-    else return /Firefox/gi.test(navigator.userAgent);
+    return navigator.userAgentData
+      ? navigator.userAgentData.brands.some(brand =>
+          brand.brand.toLowerCase().includes("firefox")
+        )
+      : /Firefox/gi.test(navigator.userAgent);
   }
   static isExplorer(): boolean {
     return /MSIE|Trident/gi.test(navigator.userAgent);
-  }
-  static isWebkit(): boolean {
-    return CompabilityValidator.isChromium() || CompabilityValidator.isSafari();
   }
 }
