75019dc781f5c338d136c37b309c453abb749bc4
Aron-PC <aron.b.96@gmail.com>
2025-01-13 15:35:34 -0300

    include only feature files

--- src/components/buttons/Submit.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { classes } from "@/lib/client/vars";
-export default function Submit() {
-  return (
-    <button type='button' id='btnSubmit' className={classes.btnPrim}>
-      Enviar
-    </button>
-  );
-}
